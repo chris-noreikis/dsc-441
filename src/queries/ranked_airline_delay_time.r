196845f739b393675242a414a980d92508763f79
Chris Noreikis <cnoreikis@stats.com>
2020-02-11 18:42:47 -0600

    Complete query to rank airlines by delay time

--- src/queries/ranked_airline_delay_time.r
+++ src/queries/ranked_airline_delay_time.r
@@ -1,26 +1,7 @@
-"YEAR"
-"MONTH"
-"OP_CARRIER"
-"ORIGIN"
-"ORIGIN_CITY_NAME"
-"ORIGIN_STATE_ABR"
-"DEST"
-"DEST_CITY_NAME"
-"DEST_STATE_ABR"
-"CRS_DEP_TIME"
-"DEP_TIME"
-"DEP_DELAY"
-"DEP_DELAY_NEW"
-"DEP_DEL15"
-"CRS_ARR_TIME"
-"ARR_TIME"
-"ARR_DELAY"
-"ARR_DEL15"
-"CANCELLED"
-"DIVERTED"
-"DISTANCE"
-"CARRIER_DELAY"
-"WEATHER_DELAY"
-"NAS_DELAY"
-"SECURITY_DELAY"
-"LATE_AIRCRAFT_DELAY"
+source("src/data_source.r")
+
+complete_dataset %>%
+  filter(!is.na(DEP_DELAY_NEW)) %>%
+  group_by(OP_CARRIER) %>%
+  summarise(avg_delay_time = mean(DEP_DELAY_NEW)) %>%
+  arrange(desc(avg_delay_time))
